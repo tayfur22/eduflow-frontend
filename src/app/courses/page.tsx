2d284feb4fc06b3d826a55f194fdefe71a7dde67
tayfur22 <tayfurtagizade23@gmail.com>
2026-05-29 13:02:07 +0400

    fronted improvementss

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useState, useCallback } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
